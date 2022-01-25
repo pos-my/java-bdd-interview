5c5d9cbd6b08158cabf931d213e6ae07f2087e37
Bhatia <shruti.bhatia@dxc.com>
2022-01-25 21:55:09 +0800

    first

--- src/main/java/posmy/interview/qa/App.java
+++ src/main/java/posmy/interview/qa/App.java
@@ -1,13 +1,39 @@
 package posmy.interview.qa;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) throws InterruptedException {
+		String text ="Top stories";
+		System.setProperty("webdriver.chrome.driver","C:\\dr\\chromedriver.exe");
+		WebDriver wd = new ChromeDriver();
+		wd.get("https://www.google.com");
+		WebElement elem = wd.findElement(By.name("q"));
+		elem.sendKeys(new String[] { "covid 19 in Malaysia" });
+		elem.submit();
+		
+		Thread.sleep(2000);
+
+		if(wd.getPageSource().contains("Top stories")){
+			System.out.println("Text is present");
+		}else{
+			System.out.println("Text is absent");
+		}
+		
+		wd.findElement(By.partialLinkText("COVID-19 MALAYSIA: Home")).click();
+		Thread.sleep(3000);
+		
+		String url = wd.getCurrentUrl();
+		System.out.println(url);
+		wd.quit();
+	}
+
 }
